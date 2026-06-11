956ae7933ecdbf9e233477973662510064263647
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-11 16:11:58 -0700

    Update urls.py

--- Restaurant/LittleLemon/myproject/myapp/urls.py
+++ Restaurant/LittleLemon/myproject/myapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.CategoryListCreateAPIView.as_view()),
-    path('products/', views.ProductsListCreateAPIView.as_view())
+    path('categories/<int:pk>/', views.CategoryView.as_view()),
 ]
