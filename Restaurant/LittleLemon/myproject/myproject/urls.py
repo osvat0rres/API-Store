73c01535aeaaf478a2536323a3147193ccce9e9f
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-11 16:11:22 -0700

    Update urls.py

--- Restaurant/LittleLemon/myproject/myproject/urls.py
+++ Restaurant/LittleLemon/myproject/myproject/urls.py
@@ -20,9 +20,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-    path('auth/',include('djoser.urls')),
-#     path('auth/', include('djoser.urls.authtoken')),
-#     path('token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
-#     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+   path('',include('myapp.urls)),
+
 ]
