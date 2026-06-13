a128ed2fc20b108141edb8ef12ba6f57481ca83b
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-12 23:39:45 -0700

    Update views.py

--- Restaurant/LittleLemon/myproject/myapp/views.py
+++ Restaurant/LittleLemon/myproject/myapp/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Category, MenuItem, Cart, Order, OrderItem
-from .serializers import CategorySerializer, MenuItemSerializer
+from .serializers import CategorySerializer, CartSerializer, MenuItemSerializer, OrderSerializers
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 from django.shortcuts import  get_object_or_404
@@ -21,4 +21,51 @@ class CategoryView(generics.ListCreateAPIView):
         if self.request.method == 'POST':
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
-        
+
+
+#This allows authenticated users to see the items in their cart and add new ones
+class CartView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Cart.objects.all().filter(user=self.request.user)
+    
+    #It allows items to be deleted form the cart
+    def delete(self, request, *args, **kwargs):
+        Cart.objects.all().filter(user=self.request.user).delete()
+        return Response("All items were deleted")
+    
+
+#This will create a new item        
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    search_fields = ['category_tittle']
+    ordering_fields = ['price']
+    
+    def get_permissions(self):
+        permission_classes = [ ]
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+    
+
+#This will allow users to rertrieve, update or delete a single item
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_class = [IsAuthenticated]
+    
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+    
+
+class OrderView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializers
+    permission_class = [IsAuthenticated]
