7b20b14432d0db38aabadf7e88ede963c5c27bc4
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-12 23:40:02 -0700

    Update serializers.py

--- Restaurant/LittleLemon/myproject/myapp/serializers.py
+++ Restaurant/LittleLemon/myproject/myapp/serializers.py
@@ -14,5 +14,41 @@ class MenuItemSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = MenuItem
-        fields = ['id', 'name', 'description', 'price', 'category']
- 
+        fields = ['id', 'tittle', 'featured', 'price', 'category']
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset= User.objects.all(),
+        default = serializers.CurrentUserDefault()
+    )
+    
+    def validate(self,atts):
+        atts['price'] = atts['quantiry'] * atts['unit_pice']
+        
+        
+    class Meta:
+        model = Cart
+        fields = ['user', "menuitem", "quantity", "price",'unit_price']
+        #This means that the client cannot provide there own value fro price
+        extra_kwargs = {'price' : {'read_only' : True}}
+        
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = [ 'order',  'menuItem', 'quantity', 'price']
+        
+        
+        
+class OrderSerializers(serializers.ModelSerializer):
+    OrderItems = OrderItemSerializer(many=True, read_only = True, source='order')
+    
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'delivery_crew', 'status','date','total', 'orderitem']
+        
+        
+class UserSericalizer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+        
