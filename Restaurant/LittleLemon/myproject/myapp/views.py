8a84e8ac91d175e0defbd5574cfb545012ed3fe1
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-14 13:37:20 -0700

    Update views.py

--- Restaurant/LittleLemon/myproject/myapp/views.py
+++ Restaurant/LittleLemon/myproject/myapp/views.py
@@ -1,71 +1,135 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Category, MenuItem, Cart, Order, OrderItem
-from .serializers import CategorySerializer, CartSerializer, MenuItemSerializer, OrderSerializers
+from .serializers import CategorySerializer, MenuItemSerializer, CartSerializer, OrderSerializers
 from rest_framework.response import Response
+
 from rest_framework.permissions import IsAdminUser
 from django.shortcuts import  get_object_or_404
+
 from django.contrib.auth.models import Group, User
+
 from rest_framework import viewsets
 from rest_framework import status
-# Create your views here.
 
-    
-#This will create a category input
-class CategoryView(generics.ListCreateAPIView):
+#This will create a category
+class CategoriesView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    
+
     def get_permissions(self):
         permission_classes = []
-        if self.request.method == 'POST':
+        if self.request.method != 'GET':
             permission_classes = [IsAuthenticated]
+
         return [permission() for permission in permission_classes]
 
-
-#This allows authenticated users to see the items in their cart and add new ones
-class CartView(generics.ListCreateAPIView):
-    queryset = Cart.objects.all()
-    serializer_class = CartSerializer
-    permission_classes = [IsAuthenticated]
-    
-    def get_queryset(self):
-        return Cart.objects.all().filter(user=self.request.user)
-    
-    #It allows items to be deleted form the cart
-    def delete(self, request, *args, **kwargs):
-        Cart.objects.all().filter(user=self.request.user).delete()
-        return Response("All items were deleted")
-    
-
-#This will create a new item        
+# Will create a menu item
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    search_fields = ['category_tittle']
-    ordering_fields = ['price']
-    
+    search_fields = ['category__title']
+    ordering_fields = ['price', 'inventory']
+
     def get_permissions(self):
-        permission_classes = [ ]
+        permission_classes = []
         if self.request.method != 'GET':
             permission_classes = [IsAuthenticated]
+
         return [permission() for permission in permission_classes]
-    
 
 #This will allow users to rertrieve, update or delete a single item
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_class = [IsAuthenticated]
-    
+
     def get_permissions(self):
         permission_classes = []
         if self.request.method != 'GET':
             permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
-    
 
+        return [permission() for permission in permission_classes]
+
+#This allows authenthicated users to see the itmes in their casrt and add new ones
+class CartView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cart.objects.all().filter(user=self.request.user)
+
+    def delete(self, request, *args, **kwargs):
+        Cart.objects.all().filter(user=self.request.user).delete()
+        return Response("ok")
+
+#This view is for managing orders
 class OrderView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializers
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    
+    #This determines which order whicch order is allocated to the user
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Order.objects.all()
+        elif self.request.user.groups.count()==0: #normal customer 
+            return Order.objects.all().filter(user=self.request.user)
+        elif self.request.user.groups.filter(name='Delivery Crew').exists(): #delivery crew
+            return Order.objects.all().filter(delivery_crew=self.request.user)  #only show oreders assigned to him
+        else: #delivery crew or manager
+            return Order.objects.all()
+    #This is when someone sands POST/order to place an order
+    def create(self, request, *args, **kwargs):
+        menuitem_count = Cart.objects.all().filter(user=self.request.user).count()
+        if menuitem_count == 0:
+            return Response({"message:": "no item in cart"})
+
+        data = request.data.copy()
+        total = self.get_total_price(self.request.user)
+        data['total'] = total
+        data['user'] = self.request.user.id
+        order_serializer = OrderSerializers(data=data)
+        if (order_serializer.is_valid()):
+            order = order_serializer.save()
+
+            items = Cart.objects.all().filter(user=self.request.user).all()
+
+            for item in items.values():
+                orderitem = OrderItem(
+                    order=order,
+                    menuitem_id=item['menuitem_id'],
+                    price=item['price'],
+                    quantity=item['quantity'],
+                )
+                orderitem.save()
+
+            Cart.objects.all().filter(user=self.request.user).delete() #Delete cart items
+
+            result = order_serializer.data.copy()
+            result['total'] = total
+            return Response(order_serializer.data)
+    
+    def get_total_price(self, user):
+        total = 0
+        items = Cart.objects.all().filter(user=user).all()
+        for item in items.values():
+            total += item['price']
+        return total
+
+class SingleOrderView(generics.RetrieveUpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializers
+    permission_classes = [IsAuthenticated]
+    
+    def update(self,request, *args, **kwargs):
+        if self.request.user.groups.count() == 0:
+            return Response('Not Ok')
+        else:
+            return super().update(request, *args, **kwargs)
+
+
+class GroupView(viewsets.ViewSet):
+    permission_classes = [IsAdminUser]
+    
+    
