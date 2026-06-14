1b8d7024a71f5441a3b99fd4fde872649c6bf958
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-14 13:38:17 -0700

    Add order path to urlpatterns in urls.py

--- Restaurant/LittleLemon/myproject/myapp/urls.py
+++ Restaurant/LittleLemon/myproject/myapp/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.CategoryView.as_view()),
-    path('cart/', views.CartView.as_view()),
+    path('categories/', views.CategoriesView.as_view()),
+    path('cart/menu-items/', views.CartView.as_view()),
     path('menuitems/', views.MenuItemsView.as_view()),
     path('menuitems/<int:pk>/', views.SingleMenuItemView.as_view()),
+    path('orders/', views.OrderView.as_view()),
 
 ]
+
+#CartegoryView
+#CartView
+#MenuItemsView
+#SingleMenuItemView
+#OrderView
